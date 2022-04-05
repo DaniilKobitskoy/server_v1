39a24ba39d9564a853e64d52810aa23f6ffdf529
DaniilKobitskoy <dkobitskoy123@mail.ru>
2022-04-05 15:40:33 +0300

    Дима посмотри

--- app/src/main/java/com/template/LoadingActivity.kt
+++ app/src/main/java/com/template/LoadingActivity.kt
@@ -1,7 +1,5 @@
 package com.template
 
-import android.R
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -20,32 +18,12 @@ import com.template.databinding.ActivityLoadingBinding
 import java.util.*
 
 
-//class SharedPreferencesExample : Activity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.main)
-//        val sp = getSharedPreferences(
-//            MY_SETTINGS,
-//            MODE_PRIVATE
-//        )
-//        // проверяем, первый ли раз открывается программа
-//        val hasVisited = sp.getBoolean("hasVisited", false)
-//        if (!hasVisited) {
-//            // выводим нужную активность
-//            val e: Editor = sp.edit()
-//            e.putBoolean("hasVisited", true)
-//            e.commit() // не забудьте подтвердить изменения
-//        }
-//    }
-//
-//    companion object {
-//        private const val MY_SETTINGS = "my_settings"
-//    }
-//}
+
 
         class LoadingActivity : AppCompatActivity() {
-
-
+            var APP_PREFERENCES = "mysettings"
+          var APP_PREFERENCES_URL: String = ""
+            var mSettings: SharedPreferences? = null
                 private val MY_SETTINGS = "my_settings"
                 private lateinit var database: DatabaseReference
                 private lateinit var baseAnalytics: FirebaseAnalytics
@@ -54,36 +32,46 @@ import java.util.*
 
             override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
+                mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
                 binding = ActivityLoadingBinding.inflate(layoutInflater)
                 setContentView(binding.root)
                 database = Firebase.database.reference
                 baseAnalytics = Firebase.analytics
-                shareppref()
+                var length: Int = APP_PREFERENCES_URL.trim().length
+
+                if (length > 0){
+                    startWebView()
+
+                }else{
+                    shareppref()
+
+                }
 
             }
-
             private fun shareppref() {
                 database.addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
                         val db = database.toString()
                         Log.d("TAG", "Value is: $database ")
                         Log.d("TAG", "Value is: $db ")
-
                         //get package name
                         val packageName = applicationContext.packageName
                         url =
                             "$database/?packageid=$packageName&usserid=${UUID.randomUUID()}&getz=${TimeZone.getDefault()}&getr=utm_source=google-play&utm_medium=organic"
-//                Log.d("url full", url)
+                        var editor: SharedPreferences.Editor = mSettings!!.edit()
+                        editor.putString(APP_PREFERENCES_URL, url)
+                        editor.apply()
+                        Log.d("URL", APP_PREFERENCES_URL)
 
-
+                        startWebView()
                     }
 
                     override fun onCancelled(error: DatabaseError) {
                         val intent = Intent(this@LoadingActivity, MainActivity::class.java)
                         intent.putExtra("url", url)
                         startActivity(intent)
+                        finish()
                     }
-
                 })
                 val sp = getSharedPreferences(
                     MY_SETTINGS,
@@ -96,15 +84,23 @@ import java.util.*
                     val e: SharedPreferences.Editor = sp.edit()
                     e.putBoolean("hasVisited", true)
                     e.commit()
-                startWebView()// не забудьте подтвердить изменения
+                    startWebView()
+               // не забудьте подтвердить изменения
                 }
 
             }
             private fun startWebView() {
                 val intent = Intent(this@LoadingActivity, WebActivity::class.java)
+                intent.putExtra("url", APP_PREFERENCES_URL)
+
             startActivity(intent)
+                finish()
             }
 
+//            override fun onStop() {
+//                APP_PREFERENCES_URL
+//                super.onStop()
+//            }
             companion object {
                 private const val MY_SETTINGS = "my_settings"
             }
